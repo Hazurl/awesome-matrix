5327362514d9d4d1ff331cb1f03f3bf7a62e855e
Hazurl <ricoujules69@gmail.com>
2018-01-07 18:41:08 +0100

    Cross product for row vectors

--- include/awm/Operations/OpCrossProduct.hpp
+++ include/awm/Operations/OpCrossProduct.hpp
@@ -5,9 +5,9 @@
 namespace awm {
 
 template<template<typename, unsigned int, unsigned int> typename M, typename T, unsigned int R, unsigned int C, bool = (
-    C == 1 && (R == 2 || R == 3)
+    (C == 1 && (R == 2 || R == 3)) || (R == 1 && (C == 2 || C == 3))
 )>
-class OpCrossProduct {
+class OpCrossProduct { // R = 3, C = 1
     using this_t = M<T, R, C>*;
     using cthis_t = const M<T, R, C>*;
     using result_t = M<T, 3, 1>;
@@ -28,6 +28,27 @@ public:
 };
 
 template<template<typename, unsigned int, unsigned int> typename M, typename T>
+class OpCrossProduct<M, T, 1, 3, true> {
+    using this_t = M<T, 1, 3>*;
+    using cthis_t = const M<T, 1, 3>*;
+    using result_t = M<T, 1, 3>;
+
+    const T& at(unsigned int i) const {
+        return static_cast<cthis_t>(this)->at(0, i);
+    }
+
+public:
+
+    result_t cross_product(M<T, 3, 1> const& v) const {
+        return {
+            at(2) * v.at(0, 3) - at(3) * v.at(0, 2),
+            at(3) * v.at(0, 1) - at(1) * v.at(0, 3),
+            at(1) * v.at(0, 2) - at(2) * v.at(0, 1)
+        };
+    }
+};
+
+template<template<typename, unsigned int, unsigned int> typename M, typename T>
 class OpCrossProduct<M, T, 2, 1, true> {
     using this_t = M<T, 2, 1>*;
     using cthis_t = const M<T, 2, 1>*;
@@ -41,6 +62,19 @@ public:
     }
 };
 
+template<template<typename, unsigned int, unsigned int> typename M, typename T>
+class OpCrossProduct<M, T, 1, 2, true> {
+    using this_t = M<T, 1, 2>*;
+    using cthis_t = const M<T, 1, 2>*;
+public:
+
+    auto cross_product(M<T, 1, 2> const& v) const {
+        return static_cast<cthis_t>(this)->at(0, 0) * 
+               v.at(0, 1) -
+               static_cast<cthis_t>(this)->at(0, 1) *
+               v.at(0, 0);
+    }
+};
 
 template<template<typename, unsigned int, unsigned int> typename M, typename T, unsigned int R, unsigned int C>
 class OpCrossProduct<M, T, R, C, false> {};
